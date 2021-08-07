ffa2cc1b4aaae3e581c3fd8ab402fe8c16d1ee1e
finnahern <finna9x@gmail.com>
2021-08-07 19:36:37 +0000

    Add functionality to sort the data by life expectancy highest to lowest, still in progress

--- run.py
+++ run.py
@@ -4,16 +4,29 @@ with open('lifeExpect.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
 
-    first_five = []
-
     def get_top_five():
+        """
+        Sort the .csv file into a list of country names and life expectancy values.
+        Sort these by life expectancy values and display the top 5.
+        """
 
-        line_count = 1
-        while line_count < 6:
-            first_five.append(data[line_count][3])
-            line_count += 1
+        print("Creating Life expectancy array...")
+        life_expect_list = []
+        for row in data[1:]:
+            line = [row[0], row[3]]
+            life_expect_list.append(line)
 
-        print(first_five)
+        life_expect_list.sort(key=lambda x: x[1], reverse=True)
+        life_expect_list[1:6]
+        print(life_expect_list)
+
+        # line_count = 1
+        # first_five = []
+        # while line_count < 6:
+        # first_five.append(data[line_count][3])
+        # line_count += 1
+
+        # print(first_five)
 
     get_top_five()
 
