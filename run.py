b769057408d018c0c55663a10ba4cddbd9deef75
finnahern <finna9x@gmail.com>
2021-08-07 18:33:24 +0000

    Add get_top_five function

--- run.py
+++ run.py
@@ -4,4 +4,18 @@ with open('lifeExpect.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
 
-print(data)
+    first_five = []
+
+    def get_top_five():
+
+        line_count = 1
+        while line_count < 6:
+            first_five.append(data[line_count][3])
+            line_count += 1
+
+        print(first_five)
+
+    get_top_five()
+
+    # for row in data:
+    # print(row[0])
