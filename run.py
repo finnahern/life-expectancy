7559a94fb47f053530c4cea579e10dd0d803038b
finnahern <finna9x@gmail.com>
2021-08-08 15:01:09 +0000

    Add main function, add docstring to validate_input function, rename get_top_five to sort_data

--- run.py
+++ run.py
@@ -26,8 +26,9 @@ with open('lifeExpect.csv', newline='') as f:
 
     def validate_input(selection):
         """
-        Check that the user input is an integer within the specified range. If not display
-        and error message and continue While loop in get_user_input.
+        Check that the user input is an integer within the specified range.
+        If not display an error message and continue While loop in
+        get_user_input.
         """
         try:
             input_int = int(selection)
@@ -40,24 +41,30 @@ with open('lifeExpect.csv', newline='') as f:
 
         return True
 
-    def get_top_five():
+    def sort_data(selection):
         """
         Sort the .csv file into a list of country names and life expectancy
         values. Sort these by life expectancy values and display the top five.
         """
 
         print("Creating Life expectancy array...")
-        life_expect_list = []
+        data_list = []
         for row in data[1:]:
             line = [row[0], row[3]]
-            life_expect_list.append(line)
+            data_list.append(line)
 
-        life_expect_list.sort(key=lambda x: x[1], reverse=True)
-        life_expect_list = life_expect_list[:5]
+        data_list.sort(key=lambda x: x[1], reverse=True)
+        data_list = data_list[:5]
 
         print("The top five countries for life expectancy in 2015 are:")
         for x in range(5):
-            print(f"{(x + 1)}. {life_expect_list[x][0]}    {life_expect_list[x][1]}")
+            print(f"{(x + 1)}. {data_list[x][0]}    {data_list[x][1]}")
 
-    get_user_input()
-    #get_top_five()
+
+def main():
+    """
+    Main function to take and pass arguments to the other functions as required
+    """
+
+    selection = get_user_input()
+    sort_data(selection)
