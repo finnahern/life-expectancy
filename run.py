41fddee88f8a93e51fb853586c4eab22e2df2d79
finnahern <finna9x@gmail.com>
2021-08-08 11:38:19 +0000

    Complete update_top_five function, output now correct

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ with open('lifeExpect.csv', newline='') as f:
 
     def get_top_five():
         """
-        Sort the .csv file into a list of country names and life expectancy values.
-        Sort these by life expectancy values and display the top 5.
+        Sort the .csv file into a list of country names and life expectancy
+        values. Sort these by life expectancy values and display the top 5.
         """
 
         print("Creating Life expectancy array...")
@@ -17,18 +17,10 @@ with open('lifeExpect.csv', newline='') as f:
             life_expect_list.append(line)
 
         life_expect_list.sort(key=lambda x: x[1], reverse=True)
-        life_expect_list[1:6]
-        print(life_expect_list)
+        life_expect_list = life_expect_list[:5]
 
-        # line_count = 1
-        # first_five = []
-        # while line_count < 6:
-        # first_five.append(data[line_count][3])
-        # line_count += 1
-
-        # print(first_five)
+        print("The top five countries for life expectancy in 2015 are:")
+        for x in range(5):
+            print(f"{(x + 1)}. {life_expect_list[x][0]}    {life_expect_list[x][1]}")
 
     get_top_five()
-
-    # for row in data:
-    # print(row[0])
