20c542b937375587bbfe71347761d97de7a215a3
finnahern <finna9x@gmail.com>
2021-08-08 15:47:18 +0000

    Begin refactoring sort_data to accept user selection as an argument and sort the requested column based on that

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ with open('lifeExpect.csv', newline='') as f:
         """
         Prompt the user to select which data they want sorted and displayed
         """
+
         while True:
             print("Please select the data you wish to sort by entering a number between 1 and 5.")
             print("1. Life Expectancy")
@@ -30,6 +31,7 @@ with open('lifeExpect.csv', newline='') as f:
         If not display an error message and continue While loop in
         get_user_input.
         """
+
         try:
             input_int = int(selection)
             if input_int > 5 or input_int < 1:
@@ -46,11 +48,32 @@ with open('lifeExpect.csv', newline='') as f:
         Sort the .csv file into a list of country names and life expectancy
         values. Sort these by life expectancy values and display the top five.
         """
+        print(f"sort_data called, selection is: {selection}")
+        #Refactor to include all columns with a for loop
+        data_category = ""
+        data_col = 0
+        if selection == 1:
+            data_category = "life expectancy"
+            data_col = 3
+        elif selection == 2:
+            data_category = "adult mortality"
+            data_col = 4
+        elif selection == 3:
+            data_category = "infant mortality"
+            data_col = 5
+        elif selection == 4:
+            data_category = "GDP"
+            data_col = 12
+        elif selection == 5:
+            data_category = "average years of schooling"
+            data_col = 15
 
-        print("Creating Life expectancy array...")
+        print(f"data_col is: {data_col}")
+        print(f"data_category is: {data_category}")
+
         data_list = []
         for row in data[1:]:
-            line = [row[0], row[3]]
+            line = [row[0], row[data_col]]
             data_list.append(line)
 
         data_list.sort(key=lambda x: x[1], reverse=True)
@@ -60,11 +83,13 @@ with open('lifeExpect.csv', newline='') as f:
         for x in range(5):
             print(f"{(x + 1)}. {data_list[x][0]}    {data_list[x][1]}")
 
+    def main():
+        """
+        Main function to take and pass arguments to the other
+        functions as required
+        """
 
-def main():
-    """
-    Main function to take and pass arguments to the other functions as required
-    """
+        selection = get_user_input()
+        sort_data(selection)
 
-    selection = get_user_input()
-    sort_data(selection)
+main()
