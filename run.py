ead24d48df6d7245d3446528724ef3534995dc95
finnahern <finna9x@gmail.com>
2021-08-13 19:34:14 +0000

    Add feature to sort_data function to show bottom 5 countries in selected category. Add comments to improve readability

--- run.py
+++ run.py
@@ -8,9 +8,10 @@ with open('lifeExpect.csv', newline='') as f:
         """
         Prompt the user to select which data they want sorted and displayed
         """
-
+        # while loop to repeat request for input until valid selection is entered.
         while True:
             print("Please select the data you wish to sort by entering a number between 1 and 13.")
+            # for loop to print list of data categories the user can choose from.
             for x in range(13):
                 print(f"{x + 1}. {data[0][x + 3]}")
 
@@ -47,18 +48,30 @@ with open('lifeExpect.csv', newline='') as f:
         """
         print(f"sort_data called, selection is: {selection}")
 
+        # create data_list out of the countries column and the column of the selected data.
         data_list = []
         for row in data[1:]:
             row[selection + 2] = float(row[selection + 2])
             line = [row[0], row[selection + 2]]
             data_list.append(line)
 
+        # sort data_list in descending order and make a list of the top 5.
         data_list.sort(key=lambda x: x[1], reverse=True)
-        data_list = data_list[:5]
+        data_list_top = data_list[:5]
 
-        print(f"The top five countries for {data[0][selection + 2]} in 2015 are:")
+        # sort data_list in ascending order and make a list of the bottom 5.
+        data_list_bottom = data_list[-5:]
+        data_list_bottom.sort(key=lambda x: x[1])
+
+        # print a list of the top 5 countries in the selected category.
+        print(f"\nThe top five countries for {data[0][selection + 2]} in 2015 are:")
         for x in range(5):
-            print(f"{(x + 1)}. {data_list[x][0]}    {data_list[x][1]}")
+            print(f"{(x + 1)}. {data_list_top[x][0]}    {data_list_top[x][1]}")
+
+        # print a list of the bottom 5 countries in the selected category.
+        print(f"\nThe bottom five countries for {data[0][selection + 2]} in 2015 are:")
+        for x in range(5):
+            print(f"{(x + 1)}. {data_list_bottom[x][0]}    {data_list_bottom[x][1]}")
 
     def main():
         """
